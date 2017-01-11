e60da1d274df0f33ea99cd8a69acb86fd0d06dc5
nick <nick@fagan.org>
2017-01-11 17:55:46 -0500

    added comparison of own and other gaze data

--- slave/slave.ino
+++ slave/slave.ino
@@ -52,21 +52,37 @@ void setup() {
 
 void loop() {
 
-  //	rewards
+  //	slave serial input
 
   if ( Serial.available() ) {
     byteRead = Serial.read();
     char readChar = toChar( byteRead );
+    //  update our OWN current gaze coordinates
+    //  if readChar corresponds to MESSAGE__EYE_START    
     if ( readChar == MESSAGE__EYE_START ) {
-      relay( readChar );
-    }
-    //	check to see if this is a reward message
-    int index = findIndex( MESSAGES__REWARDS, N_REWARDS, readChar );
-    if ( index != -1 ) {
-      deliverReward( index, REWARD_SIZE );
+      String eyePosition = "";
+      while ( Serial.available() == 0 ) {
+        delay(5);
+      }
+      while ( Serial.available() > 0 ) {
+        int pos = Serial.read();
+        if ( pos == MESSAGE__EYE_END ) {
+          break;
+        } else {
+          eyePosition += (char)pos;
+        }
+      }
+      updateEyePosition( eyePosition, true );
+    } else if ( readChar == 'P' ) {
+      compareOwnToOtherPosition();
+    } else {
+      //  check to see if this is a reward message
+      int index = findIndex( MESSAGES__REWARDS, N_REWARDS, readChar );
+      if ( index != -1 ) {
+        deliverReward( index, REWARD_SIZE );
+      }
     }
   }
-
 }
 
 void relay( char c ) {
@@ -107,11 +123,29 @@ void handleReceipt( int nBytes ) {
     char pos = Wire.read();
     eyePosition += pos;
   }
-
+  
   //  eyePosition[0] will be the
   //  X or Y target. Update the corresponding
   //  OTHER array accordingly
 
+  updateEyePosition( eyePosition, false );
+  
+}
+
+void compareOwnToOtherPosition() {
+  bool isWithinBounds[ 2 ] = { false, false };
+  for ( int i = 0; i < 2; i++ ) {
+    isWithinBounds[i] = abs( OWN[i] - OTHER[i] ) <= BOUNDS;
+    if ( !isWithinBounds[i] ) {
+      Serial.println( "OOB" );
+      return;
+    }
+  }
+  //  otherwise, we're in bounds
+  Serial.println( "IB" );
+}
+
+void updateEyePosition( String eyePosition, bool isOwn ) {
   char positionID = eyePosition.charAt( 0 );
   bool valid = false;
   int posIndex;
@@ -122,19 +156,28 @@ void handleReceipt( int nBytes ) {
     valid = true;
     posIndex = 1;
   }
-
-  if ( valid ) {
-    int bufferSize = eyePosition.length() + 1;
-    char charNumber[ bufferSize ] = { 'b' };
-    //  get rid of the X or Y
-    eyePosition.remove(0, 1);
-    eyePosition.toCharArray( charNumber, bufferSize );
-    OTHER[ posIndex ] = atol( charNumber );
-    Serial.println( OTHER[ posIndex ] );
-  } else {
+  if ( !valid ) {
     Serial.println( '!' ); 
+    return;
   }
+  if ( isOwn ) {
+    OWN[ posIndex ] = stringEyePositionToInt( eyePosition );
+    Serial.println( "OWN" );
+    Serial.println( OWN[ posIndex ] );
+  } else {
+    OTHER[ posIndex ] = stringEyePositionToInt( eyePosition );
+    Serial.println( "OTHER" );
+    Serial.println( OTHER[ posIndex ] );
+  }
+}
 
+int stringEyePositionToInt( String eyePosition ) {
+  int bufferSize = eyePosition.length() + 1;
+  char charNumber[ bufferSize ] = { 'b' };
+  //  get rid of the X or Y
+  eyePosition.remove(0, 1);
+  eyePosition.toCharArray( charNumber, bufferSize );
+  return atol( charNumber );
 }
 
 void handleRequest() {
